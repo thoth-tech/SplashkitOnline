0673100d54dfce4f9f5188ca46e5d3d5c02b11f6
WhyPenguins <sean@whypenguins.com>
2023-11-25 05:57:17 +1100

    feat: connect treeview to MEMFS + restructure code

--- Browser_IDE/fileview.js
+++ Browser_IDE/fileview.js
@@ -1,12 +1,237 @@
 "use strict";
 
-function nodeExists(path)
-{
+// ------- Utility Functions --------
+// function 'contains' from Aleksandr Makov - https://stackoverflow.com/a/47485168
+// How does this work??
+function contains(parent, child) {
+  return parent !== child && parent.contains(child);
+}
+// Node Utility Functions
+function isNode(node){
+    return node.classList.contains("node");
+}
+function isDirectory(node){
+    return node.classList.contains("directory");
+}
+function getParentDirectory(node){
+    if (node.dataset.label=="" || node.dataset.label==undefined)
+        return null;
+    return node.parentElement.parentElement;
+}
+function getDirectoryContents(node){
+    return node.children[1];
+}
+function getFilename(node){
+    return node.dataset.label;
+}
+function getFullDirectoryPath(node){
+    let ret = "";
+    let dir = getParentDirectory(node);
+    while(dir!=null){
+        ret = getFilename(dir)+"/"+ret;
+        dir = getParentDirectory(dir);
+    }
+    return ret;
+}
+function getFullPath(node){
+    return getFullDirectoryPath(node)+getFilename(node);
+}
+function getChildNodeWithFilename(root, name){
+    for(let child of getDirectoryContents(root).children){
+        if (getFilename(child)==name)
+            return child;
+    }
+    return null;
+}
+function getNodeFromPath(path){
+    let dir = document.getElementById("fileView");
+
+    for (let part of splitPath(path)){
+        dir = getChildNodeWithFilename(dir, part);
+        if (dir==null)
+            return null;
+    }
+    return dir;
+}
+
+// String Path Utility Functions
+function pathDirName(path){
+    return path.substring(0, path.lastIndexOf("/"));
+}
+function pathFileName(path){
+    return path.substring(path.lastIndexOf("/")+1);
+}
+function pathFileExtension(path){
+    return path.substring(path.lastIndexOf(".")+1);
+}
+function splitPath(path){
+    return path.split("/").slice(1);
+}
+
+// TreeView Utility Functions
+function moveNodeToPathUI(node, newPath, index){
+    if (!isViewablePath(newPath))return;
+    let new_dir_node = getNodeFromPath(pathDirName(newPath));
+
+    if (index == -1 || index >= getDirectoryContents(new_dir_node).children.length)
+        getDirectoryContents(new_dir_node).appendChild(node);
+    else
+        getDirectoryContents(new_dir_node).insertBefore(node, getDirectoryContents(new_dir_node).children[index]);
+}
+
+// File System Utility Functions
+function FSFileExists(path){
     try{FS.lookupPath(path, { follow: false }).node; return true;}
     catch(e) {return false;}
 }
+function scanFilesystem(path){
+    let dirs_files = FS.readdir(path).slice(2);
+
+    let nodes = []
+    for(let thing of dirs_files){
+        if (thing == "fd")
+            continue;
+        let abs_path = path+""+thing;
+
+        let info = FS.stat(abs_path);
+        let node = new Map();
+        node.set("label", thing);
+        node.set("path", abs_path);
+        node.set("children", null);
+        if (FS.isDir(info.mode)){
+            node.set("children", scanFilesystem(abs_path+"/"));
+        }
+        nodes.push(node);
+    }
+    return nodes;
+}
+function isViewablePath(path){
+    if (path.startsWith("/tmp"))return false;
+    if (path.startsWith("/home"))return false;
+    if (path.startsWith("/dev"))return false;
+    if (path.startsWith("/proc"))return false;
+    return true;
+}
 
 
+// File System UI Node Creation Functions
+function initFileNodeCallbacks(_node)
+{
+    let node = _node;
+    node.addEventListener("mousedown", async function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        isDragPotential = true;
+
+        mouseStart[0] = e.clientX;
+        mouseStart[1] = e.clientY;
+
+        let offsets = node.getBoundingClientRect();
+        dragOffset[0] = offsets.left;
+        dragOffset[1] = offsets.top;
+
+        draggedNode = node;
+    });
+}
+
+function makeDirectoryNode(label){
+    let dir_node = document.createElement("div");
+    dir_node.classList.add("node");
+    dir_node.classList.add("directory");
+    dir_node.dataset.label = label;
+
+    let dir_node_label = document.createElement("div");
+    dir_node_label.classList.add("node-label", "bi-folder2-open");
+
+    let dir_node_label_text = document.createTextNode(label);
+
+    let dir_node_contents = document.createElement("div");
+    dir_node_contents.classList.add("directory-contents", "open-directory");
+
+
+    dir_node_label.appendChild(dir_node_label_text);
+    dir_node.appendChild(dir_node_label);
+    dir_node.appendChild(dir_node_contents);
+
+    // Handle showing/hiding the folder's contents
+    // Uses a combination of CSS transitions + some Javascript
+    // hacks to make it function correctly.
+    dir_node.addEventListener("click", async function (e) {
+        let isOpen = dir_node_contents.classList.contains("open-directory");
+        let newState = !isOpen;
+        if (newState == false){
+            dir_node_contents.classList.remove("open-directory");
+            dir_node_contents.classList.add("closed-directory");
+            dir_node_contents.style.maxHeight = dir_node.children[1].scrollHeight+"px";
+            window.getComputedStyle(dir_node.children[1]).maxHeight; // Hack to force it to immediately update its height
+            dir_node_contents.style.maxHeight = "0px"; // This then becomes the target height to transition to
+            dir_node_label.classList.remove("bi-folder2-open");
+            dir_node_label.classList.add("bi-folder2");
+        }
+        else{
+            dir_node_contents.classList.add("open-directory");
+            dir_node_contents.classList.remove("closed-directory");
+            dir_node_contents.style.maxHeight = dir_node.children[1].scrollHeight+"px";
+            dir_node_label.classList.add("bi-folder2-open");
+            dir_node_label.classList.remove("bi-folder2");
+            setTimeout(function(){
+                isOpen = dir_node.children[1].classList.contains("open-directory");
+                if (isOpen)
+                    dir_node.children[1].style.maxHeight = 'initial';
+            }, 320);
+        }
+        e.stopPropagation();
+    });
+
+    initFileNodeCallbacks(dir_node);
+    return dir_node;
+}
+
+function makeFileNode(label){
+    let extensionLookup = {"json":"json","ogg":"sound", "mp3":"sound", "wav":"sound", "m4a":"sound", "png":"image", "jpg":"image", "bmp":"image"};
+    let iconLookup = {"sound":"bi-file-music", "image":"bi-file-image", "json":"bi-filetype-json", "unknown":"bi-file"};
+
+    let file_node_label = document.createElement("div");
+    file_node_label.classList.add("node", "file", "node-label");
+    file_node_label.dataset.label = label;
+
+    let fileType = extensionLookup[pathFileExtension(label)];
+    if (fileType==null||fileType==undefined)
+        fileType = "unknown";
+    file_node_label.classList.add(iconLookup[fileType]);
+
+    let file_node_label_text = document.createTextNode(label);
+
+    file_node_label.appendChild(file_node_label_text);
+
+    file_node_label.addEventListener("click", async function (e) {
+        e.stopPropagation();
+    });
+
+    initFileNodeCallbacks(file_node_label);
+    return file_node_label;
+}
+
+function populatefileView(files, root){
+    for (let file of files){
+        if (!isViewablePath(file.get("path")))continue;
+        if (file.get("children")!=null)
+        {
+            let dir_node = makeDirectoryNode(file.get("label"));
+            root.appendChild(dir_node);
+            populatefileView(file.get("children"), dir_node.children[1]);
+        }
+        else
+        {
+            let file_node_label = makeFileNode(file.get("label"));
+            root.appendChild(file_node_label);
+        }
+    }
+}
+
+
+// Drag&Drop Handling Functions, Variables and Events
 let isDragPotential = false;
 let mouseStart = [0,0];
 let dragOffset = [0,0];
@@ -14,136 +239,153 @@ let draggedNode = null;
 let draggedClone = null;
 let isDragging = false;
 let dragDist = 4*4;
-let currentTarget = null;
+
 let insertionPoint = document.createElement("div");
 insertionPoint.classList.add("insertion-point-display");
 
-for (let node of document.getElementsByClassName("directory"))
-{
-    node.addEventListener("click", async function (e) {
-        //alert("clicked" + node.text);
-        let isOpen = node.children[1].classList.contains("open-directory");
-        let newState = !isOpen;
-        if (newState == false)
-        {
-            node.children[1].classList.remove("open-directory");
-            node.children[1].classList.add("closed-directory");
-            node.children[1].style.maxHeight = node.children[1].scrollHeight+"px";
-            window.getComputedStyle(node.children[1]).maxHeight;
-            node.children[1].style.maxHeight = "0px";
-            node.children[0].classList.remove("bi-folder2-open");
-            node.children[0].classList.add("bi-folder2");
-        }
-        else
-        {
-            node.children[1].classList.add("open-directory");
-            node.children[1].classList.remove("closed-directory");
-            node.children[1].style.maxHeight = node.children[1].scrollHeight+"px";
-            node.children[0].classList.add("bi-folder2-open");
-            node.children[0].classList.remove("bi-folder2");
-            setTimeout(function(){
-                isOpen = node.children[1].classList.contains("open-directory");
-                if (isOpen)
-                    node.children[1].style.maxHeight = 'initial';
-            }, 320);
-        }
-        e.stopPropagation();
-    });
-}
-for (let node of document.getElementsByClassName("file"))
-{
-    node.addEventListener("click", async function (e) {
-        e.stopPropagation();
-    });
-}
-for (let node of document.getElementsByClassName("node"))
-{
-    node.addEventListener("mousedown", async function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        isDragPotential = true;
-        mouseStart[0] = e.clientX;// + node.offsetLeft;
-        mouseStart[1] = e.clientY;// + node.offsetTop;
-        let offsets = node.getBoundingClientRect();
-        dragOffset[0] = offsets.left;//node.offsetLeft;
-        dragOffset[1] = offsets.top;//node.offsetTop;
-        draggedNode = node;
-    });
-}
-
-// How does this work??
-// function 'contains' from Aleksandr Makov - https://stackoverflow.com/a/47485168
-function contains(parent, child) {
-  return parent !== child && parent.contains(child);
-}
-
-function dropElementInNodeTree(cx,cy,elem)
-{
-    for(let under of document.elementsFromPoint(cx, cy))
-    {
-        if (under.classList.contains("node") && !contains(draggedNode, under) && !contains(draggedClone, under) && draggedClone != under)
-        {
-            let isDir = under.classList.contains("directory");
+function getNewPathInTree(cx,cy,elem){
+    for(let under of document.elementsFromPoint(cx, cy)){
+        if (isNode(under) && !contains(draggedNode, under) && !contains(draggedClone, under) && draggedClone != under){
+            let isDir = isDirectory(under);
             let label = under;
             if (isDir)
                 label = under.children[0];
             let offsets = label.getBoundingClientRect();
             let midpoint = offsets.top + offsets.height/2;
-            if (cy < midpoint || draggedNode == under)
-            {
-                under.parentElement.insertBefore(elem, under);
-            }
-            else
-            {
-                if (isDir)
-                    under.children[1].insertBefore(elem, under.children[1].children[0]);
-                else
-                    under.insertAdjacentElement("afterend", elem);
-            }
 
-            console.log(cy < midpoint);
+            let dropPos = "/";
+            let dropIndex = -1;
 
-            break;
+            if (cy < midpoint || draggedNode == under){
+                dropPos = getFullDirectoryPath(under)+getFilename(elem);
+                dropIndex = [].indexOf.call(under.parentNode.children, under);
+            }
+            else{
+                if (isDir){
+                    dropPos = getFullPath(under)+"/"+elem.dataset.label;
+                    dropIndex = 0;
+                }
+                else{
+                    dropPos = getFullDirectoryPath(under)+elem.dataset.label;
+                    dropIndex = [].indexOf.call(under.parentNode.children, under)+1;
+                }
+            }
+            return {path:dropPos, index:dropIndex};
         }
     }
+    return "/"+elem.dataset.label;
 }
+
+
 document.addEventListener("mousemove", function(e) {
-    if (isDragPotential || isDragging)
-    {
+    if (isDragPotential || isDragging){
         let cx = e.clientX;
-        let mx = mouseStart[0];//-dragOffset[0];
+        let mx = mouseStart[0];
         let cy = e.clientY;
-        let my = mouseStart[1];//-dragOffset[1];
-        if (!isDragging && (cx-mx)*(cx-mx) + (cy-my)*(cy-my) > dragDist)
-        {
+        let my = mouseStart[1];
+
+        // Check if should begin dragging
+        if (!isDragging && (cx-mx)*(cx-mx) + (cy-my)*(cy-my) > dragDist){
+            // If so, create a clone of the dragged object
             isDragging = true;
-            //draggedNode.style.position = 'relative';
             draggedClone = draggedNode.cloneNode(true);
             document.body.appendChild(draggedClone);
             draggedClone.classList.add('in-drag');
         }
-        if (isDragging)
-        {
-            //console.log("dragging"+(draggedNode.offsetLeft - parseInt(draggedNode.style.left, 10)));
-            draggedClone.style.left = (cx-mx+dragOffset[0])+"px";//+dragOffset[0]
-            draggedClone.style.top = (cy-my+dragOffset[1])+"px";//+dragOffset[1]
 
-            dropElementInNodeTree(cx, cy, insertionPoint);
+        // If dragging, update the position of the clone and move the
+        // insertionPoint line to the current insertion point.
+        if (isDragging){
+            draggedClone.style.left = (cx-mx+dragOffset[0])+"px";
+            draggedClone.style.top = (cy-my+dragOffset[1])+"px";
+
+            let newPath = getNewPathInTree(cx,cy,insertionPoint);
+            moveNodeToPathUI(insertionPoint, newPath.path, newPath.index);
         }
 
     }
 });
 
 document.addEventListener("mouseup", function(e) {
-    if (isDragging)
-    {
+    if (isDragging){
         let cx = e.clientX;
         let cy = e.clientY;
-        dropElementInNodeTree(cx, cy, draggedNode);
+
+        let newPath = getNewPathInTree(cx,cy,draggedNode);
+        let oldPath = getFullPath(draggedNode);
+
+        // Perform the rename, then reposition the draggedNode to the correct index
+        FS.rename(oldPath, newPath.path);
+        draggedNode.remove();
+        moveNodeToPathUI(draggedNode, newPath.path, newPath.index);
+
+        // Tidy up drag
         insertionPoint.remove();
         draggedClone.remove();
         draggedClone = null;
+        e.stopPropagation();
     }
     isDragPotential = false;
     isDragging = false;
+});
+
+
+// File System and File System View Initialization
+moduleEvents.addEventListener("onRuntimeInitialized", function() {
+
+    // Initialize file view with existing folders/files
+    populatefileView(scanFilesystem("/"), document.getElementById("fileView").children[1]);
+
+    // Attach to file system callbacks
+    FS.trackingDelegate['onMovePath'] = function(oldpath, newPath) {
+        moveNodeToPathUI(getNodeFromPath(oldpath), newPath, -1);
+    };
+    FS.trackingDelegate['onMakeDirectory'] = function(path, mode) {
+        if (!isViewablePath(path))return;
+
+        let new_dir_node = getNodeFromPath(pathDirName(path));
+
+        let dir_node = makeDirectoryNode(pathFileName(path), path);
+        getDirectoryContents(new_dir_node).appendChild(dir_node);
+
+    };
+    FS.trackingDelegate['onDeletePath'] = function(path) {
+        if (!isViewablePath(path))return;
+
+        let new_dir_node = getNodeFromPath(pathDirName(path));
+        new_dir_node.remove();
+    };
+    FS.trackingDelegate['onOpenFile'] = function(path, flags) {
+        if (!path.startsWith("/"))
+            path = "/"+path;
+        if (!isViewablePath(path))return;
+
+        let new_dir_node = getNodeFromPath(path);
+        if (new_dir_node!=null)
+            return;
+
+        new_dir_node = getNodeFromPath(pathDirName(path));
+        let dir_node = makeFileNode(pathFileName(path), path);
+        getDirectoryContents(new_dir_node).appendChild(dir_node);
+    };
+
+    // Create default folders
+    if (!FSFileExists("/Resources"))
+        FS.mkdir("/Resources");
+    if (!FSFileExists("/Resources/animations"))
+        FS.mkdir("/Resources/animations");
+    if (!FSFileExists("/Resources/bundles"))
+        FS.mkdir("/Resources/bundles");
+    if (!FSFileExists("/Resources/databases"))
+        FS.mkdir("/Resources/databases");
+    if (!FSFileExists("/Resources/fonts"))
+        FS.mkdir("/Resources/fonts");
+    if (!FSFileExists("/Resources/images"))
+        FS.mkdir("/Resources/images");
+    if (!FSFileExists("/Resources/json"))
+        FS.mkdir("/Resources/json");
+    if (!FSFileExists("/Resources/server"))
+        FS.mkdir("/Resources/server");
+    if (!FSFileExists("/Resources/sounds"))
+        FS.mkdir("/Resources/sounds");
 });
