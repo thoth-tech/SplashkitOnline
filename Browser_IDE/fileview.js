63329010774df26959a846c3e2bf9c7f792b9c4e
jessbdeakin <s222317449@deakin.edu.au>
2024-04-24 22:03:52 +1000

    Added fileDeleteRequest and folderDeleteRequest event handlers; persistent only

--- Browser_IDE/fileview.js
+++ Browser_IDE/fileview.js
@@ -20,6 +20,15 @@ myTreeView.addEventListener("folderUploadRequest", function(e){
     document.getElementById("fileuploader").click();
 });
 
+myTreeView.addEventListener("fileDeleteRequest", function(e){
+	if (e.FS.includes("persistent"))
+        storedProject.access((project)=>project.unlink(e.path));
+});
+
+myTreeView.addEventListener("folderDeleteRequest", function(e){
+	if (e.FS.includes("persistent"))
+        storedProject.access((project)=>project.rmdir(e.path, true));
+});
 
 // Attach to file system callbacks within the Execution Environment
 executionEnviroment.addEventListener('onMovePath', function(e) {
