9a3a93e1e5f71fcedbfd6087f98ded8c1d44edf1
Sean Boettger <sean@whypenguins.com>
2024-05-15 21:51:51 +1000

    new: bi-directional communication between ExEnv and ExEnv internal  + prepare FS functions

--- Browser_IDE/runtimes/ExecutionEnvironmentInternal.js
+++ Browser_IDE/runtimes/ExecutionEnvironmentInternal.js
@@ -7,50 +7,71 @@ class ExecutionEnvironmentInternal {
     constructor(listenOn) {
         const self = this;
         // ------ Message Listening ------
-        listenOn.addEventListener('message', function(m){
-            switch(m.data.type){
-                // --- Code Execution Functions ---
-                case "HotReloadFile":
-                    self.hotReloadFile(m.data.name, m.data.code);
-                    break;
+        listenOn.addEventListener('message', async function(m){
+            try {
+                switch(m.data.type){
+                    // --- Code Execution Functions ---
+                    case "HotReloadFile":
+                        self.hotReloadFile(m.data.name, m.data.code);
+                        break;
 
-                case "ReportError":
-                    self.ReportError(userCodeBlockIdentifier + m.data.block, m.data.message, m.data.line, m.data.formatted);
-                    break;
+                    case "ReportError":
+                        self.ReportError(userCodeBlockIdentifier + m.data.block, m.data.message, m.data.line, m.data.formatted);
+                        break;
 
-                case "WriteToTerminal":
-                    self.WriteToTerminal(m.data.message);
-                    break;
+                    case "WriteToTerminal":
+                        self.WriteToTerminal(m.data.message);
+                        break;
 
-                case "CleanEnvironment":
-                    self.resetExecutionScope();
-                    break;
+                    case "CleanEnvironment":
+                        self.resetExecutionScope();
+                        break;
 
-                case "RunProgram":
-                    self.runProgram(m.data.program);
-                    break;
-                case "PauseProgram":
-                    self.pauseProgram();
-                    break;
-                case "ContinueProgram":
-                    self.continueProgram();
-                    break;
-                case "StopProgram":
-                    self.stopProgram();
-                    break;
+                    case "RunProgram":
+                        await self.runProgram(m.data.program);
+                        break;
+                    case "PauseProgram":
+                        await self.pauseProgram();
+                        break;
+                    case "ContinueProgram":
+                        await self.continueProgram();
+                        break;
+                    case "StopProgram":
+                        await self.stopProgram();
+                        break;
 
-                // --- FS Handling ---
-                case "mkdir":
-                    self.mkdir(m.data.path);
-                    break;
+                    case "callback":
+                        executeTempCallback(m.data);
+                        break;
 
-                case "writeFile":
-                    self.writeFile(m.data.path,m.data.data);
-                    break;
+                    // --- FS Handling ---
+                    case "mkdir":
+                        await self.mkdir(m.data.path);
+                        break;
 
-                case "rename":
-                    self.rename(m.data.oldPath,m.data.newPath);
-                    break;
+                    case "writeFile":
+                        await self.writeFile(m.data.path,m.data.data);
+                        break;
+
+                    case "rename":
+                        await self.rename(m.data.oldPath,m.data.newPath);
+                        break;
+
+                    case "rmdir":
+                        await self.rmdir(m.data.path, m.data.recursive);
+                        break;
+
+                    case "unlink":
+                        await self.unlink(m.data.path);
+                        break;
+                }
+
+                resolveMessageFallible(m, undefined, parent);
+            } catch(err){
+                // TODO: Do anything other than this.
+                err = err.toString();
+                rejectMessageFallible(m, err, parent);
+                throw err;
             }
         });
     }
@@ -61,17 +82,19 @@ class ExecutionEnvironmentInternal {
     signalPaused()  { parent.postMessage({type:"programPaused"},"*"); }
     signalContinue(){ parent.postMessage({type:"programContinued"},"*"); }
 
-    sendProgram(program)                { throw new Error("Unhandled sendProgram");}
-    hotReloadFile(name, code)           { throw new Error("Unhandled hotReloadFile");}
-    resetExecutionScope()               { throw new Error("Unhandled resetExecutionScope");}
-    async runProgram(program)           { throw new Error("Unhandled runProgram");}
-    async pauseProgram()                { throw new Error("Unhandled pauseProgram");}
-    async continueProgram()             { throw new Error("Unhandled continueProgram");}
-    async stopProgram()                 { throw new Error("Unhandled stopProgram");}
-    mkdir(path)                         { throw new Error("Unhandled mkdir");}
-    writeFile(path, data)               { throw new Error("Unhandled writeFile");}
-    rename(oldPath, newPath)            { throw new Error("Unhandled rename");}
-    initializeFilesystem(folders, files){ throw new Error("Unhandled initializeFilesystem");}
+    sendProgram(program)                      { throw new Error("Unhandled sendProgram");}
+    hotReloadFile(name, code)                 { throw new Error("Unhandled hotReloadFile");}
+    resetExecutionScope()                     { throw new Error("Unhandled resetExecutionScope");}
+    async runProgram(program)                 { throw new Error("Unhandled runProgram");}
+    async pauseProgram()                      { throw new Error("Unhandled pauseProgram");}
+    async continueProgram()                   { throw new Error("Unhandled continueProgram");}
+    async stopProgram()                       { throw new Error("Unhandled stopProgram");}
+    async mkdir(path)                         { throw new Error("Unhandled mkdir");}
+    async rmdir(path, recursive)              { throw new Error("Unhandled rmdir");}
+    async writeFile(path, data)               { throw new Error("Unhandled writeFile");}
+    async unlink(path)                        { throw new Error("Unhandled unlink");}
+    async rename(oldPath, newPath)            { throw new Error("Unhandled rename");}
+    async initializeFilesystem(folders, files){ throw new Error("Unhandled initializeFilesystem");}
 
     ReportError(block, message, line, formatted) {
         ReportError(block, message, line, formatted); // call external function
