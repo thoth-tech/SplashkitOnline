5e3dc215639b288b12c5b34795fcfad7da2d6915
Sean Boettger <sean@whypenguins.com>
2024-05-17 01:17:02 +1000

    fix: ensure "InitializeLanguage" doesn't occur multiple times  - the root cause of this should be found and fixed,    just don't have time for it right now, hence this stop-gap

--- Browser_IDE/runtimes/ExecutionEnvironmentInternalLoader.js
+++ Browser_IDE/runtimes/ExecutionEnvironmentInternalLoader.js
@@ -1,7 +1,15 @@
 "use strict";
 
+let hasInitializedLanguage = false;
+
 window.addEventListener('message', function(m){
     if (m.data.type == "InitializeLanguage"){
+        if (hasInitializedLanguage){
+            console.error("Double call to InitializeLanguage, please investigate and fix!");
+            return;
+        }
+        hasInitializedLanguage = true;
+
         console.log("Initializing with " + m.data.languageName);
         for (let script of m.data.runtimeFiles){
             var s = document.createElement("script");
