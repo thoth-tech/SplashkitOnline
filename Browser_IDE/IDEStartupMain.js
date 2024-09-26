051952da28b67cb5921ffa943ea5535f8770dec8
Sean Boettger <sean@whypenguins.com>
2024-09-27 02:54:00 +1000

    Fix code editors not loading on IDE startup  - regression from pull request #88

--- Browser_IDE/IDEStartupMain.js
+++ Browser_IDE/IDEStartupMain.js
@@ -43,6 +43,7 @@ async function StartIDE() {
                 (async () => {
                     await appStorage.attach();
                     await storedProject.attachToProject();
+                    openCodeEditors();
                 })()
             ])
 
