1be1353043d101e43d7c02da79b9e9a920444e0f
nielsent478 <nielsent478@gmail.com>
2025-04-17 04:47:41 +1000

    Add applyTheme support for overriding existing CSS variables

---
+++ Browser_IDE/Javascript/themes.js
@@ -0,0 +1,21 @@
+//Applies a theme by overriding CSS variables using a JSON object
+function applyTheme(theme) {
+    for (const key in theme) {
+      //Apply each color variable to the root of the document
+      document.documentElement.style.setProperty(`--${key}`, theme[key]);
+    }
+}
+  
+//example
+//const darkTheme = {
+    //'background-color': '#1e1e1e',
+    //'text-color': '#ffffff',
+    //'primary-color': '#3498db'
+//};
+  
+//Call the the function with the example
+//applyTheme(darkTheme);
+  
+
+//Run it in console dynamically after hosted
+window.applyTheme = applyTheme;
