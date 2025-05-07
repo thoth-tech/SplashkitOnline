cd84fb273cc4fb99863bc862f701c1c848e6b9b9
Sean Boettger <sean@whypenguins.com>
2025-05-07 10:11:27 +1000

    New: Add code hinter window  - Shows when typing  - Ctrl-Space can trigger manually, Escape hides it  - Some other small related UI adjustments

--- Browser_IDE/splashkit-javascript-hint.js
+++ Browser_IDE/splashkit-javascript-hint.js
@@ -13,6 +13,17 @@ function loadSplashKitAutocompletes() {
         if (xhr.readyState === 4) {
             if (xhr.status === 0 || xhr.status === 200) {
                 splashKitAutocompletes = JSON.parse(xhr.responseText);
+
+                // Sort the functions by name -> argument count
+                splashKitAutocompletes.functions.sort((a,b) => {
+                    let nameComparison = a.name.localeCompare(b.name);
+
+                    if (nameComparison == 0)
+                        return a.params.length - b.params.length;
+
+                    return nameComparison
+                });
+
             }
             else{
                 throw new Error("Couldn't load SplashKit Autocompletes!");
@@ -166,16 +177,12 @@ loadSplashKitAutocompletes();
 
 
     // Sean Edit: Handle Splashkit Functions specially
-    // TODO: Show when writing parameters, and bold current parameter
     for (func of splashKitAutocompletes.functions){
         if (func.name.lastIndexOf(start, 0) == 0 && !arrayContains(found, func.name)){
             paramList = ""
             for (param of func.params)
                 paramList += param+", "
-            found.push({
-                text: func.name,
-                displayText: (func.return!="" ? func.return + " " : "") + func.name + "(" + paramList.slice(0, paramList.length - 2) + ")"
-            });
+            found.push(func.name);
         }
     }
 
