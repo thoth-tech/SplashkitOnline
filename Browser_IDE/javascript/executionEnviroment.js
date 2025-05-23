76568255eced71dbaec2bacfee55ba1687eac381
oliver <oliver.quail3@gmail.com>
2025-05-23 15:15:57 +1000

    Forget to commit this prior to comment

--- Browser_IDE/javascript/executionEnviroment.js
+++ Browser_IDE/javascript/executionEnviroment.js
@@ -1,9 +1,1 @@
 
-let canvas = document.getElementById("canvas")
-
-window.onresize = () => {
-    if (window.innerWidth <= 600) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    }
-}
